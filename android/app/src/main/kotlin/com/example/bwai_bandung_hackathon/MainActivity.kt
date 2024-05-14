e936c854283d923dfba04b850f2d109c73de6845
andikadeveloper <muhammadandika@refactory.id>
2024-05-15 05:33:23 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/bwai_bandung_hackathon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bwai_bandung_hackathon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bwai_bandung_hackathon/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Build with AI Bandung Hackathon',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
